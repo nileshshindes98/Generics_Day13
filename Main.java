a1e5e1a233b2938117ed822e249e43633be66864
Nilesh Shinde <nileshshindes98@gmail.com>
2023-07-02 23:41:11 +0530

    Added : UC1 check test case

--- Main.java
+++ Main.java
@@ -4,15 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Max_problem mp = new Max_problem();
 
-        // Test case 1: Maximum number at 1st position
+
         Integer max1 = mp.findMaximum(10, 5, 3);
         System.out.println("Maximum: " + max1);
+        //Maximum number at 1st position
+        if (max1.equals(10)) {
+            System.out.println("Condition Passed");
+        } else {
+            System.out.println("Condition Failed ");
+        }
 
-        // Test case 2: Maximum number at 2nd position
+        // Maximum number at 2nd position
         Integer max2 = mp.findMaximum(7, 15, 4);
         System.out.println("Maximum: " + max2);
 
-        // Test case 3: Maximum number at 3rd position
+        // Maximum number at 3rd position
         Integer max3 = mp.findMaximum(2, 8, 12);
         System.out.println("Maximum: " + max3);
     }
